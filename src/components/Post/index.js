63394bd0dfc4dc787093357694bed83bf13b5ea9
f_gillet <florian.gillet@estiam.com>
2020-02-26 19:24:11 +0100

    correction des fichiers de fausses data

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -5,56 +5,27 @@ import HeaderPost from "../HeaderPost/index";
 import BodyPost from "../BodyPost/index";
 import postData from "../../data/postData.json";
 import commentsData from "../../data/commentsData.json";
-console.log(postData[0].comments);
 
-// class Post extends React.Component {
-//   render() {
-
-//       return (
-//         postData.map((postDetail, index) => {
-//         <div class="card centre" id="card_style">
-//           <HeaderPost
-//             userImg={postDetail.userImg}
-//             userName={postDetail.userName}
-//             date={postDetail.date}
-//           ></HeaderPost>
-//           <BodyPost
-//             postImg={postDetail.postImg}
-//             nbLike={postDetail.nbLike}
-//             description={postDetail.description}
-//           ></BodyPost>
-//         </div>
-//         });
-//       );
-
-//   }
-// }
-// export default Post;
-
-export default props => {
-  return (
-    <>
-      {postData.map(postDetail => (
-        <div class="card centre" id="card_style">
-          <HeaderPost
-            userImg={postDetail.userImg}
-            userName={postDetail.userName}
-            date={postDetail.date}
-          ></HeaderPost>
-          <BodyPost
-            postImg={postDetail.postImg}
-            nbLike={postDetail.nbLike}
-            description={postDetail.description}
-          ></BodyPost>
-          {/* <div style={{ padding: 20, alignItems: "center", marginBottom: 10 }}> */}
-          {commentsData.map(comments => (
-            <Comments
-              img={comments.userImgComment}
-              comment={comments.textComment}
-            ></Comments>
-          ))}
-        </div>
-      ))}
-    </>
-  );
-};
+class Post extends React.Component {
+  render() {
+    return (
+      postData.map((postDetail, index) => {
+        return (
+          <div class="card centre" id="card_style">
+            <HeaderPost
+              userImg={postDetail.userImg}
+              userName={postDetail.userName}
+              date={postDetail.date}
+            ></HeaderPost>
+            <BodyPost
+              postImg={postDetail.postImg}
+              nbLike={postDetail.nbLike}
+              description={postDetail.description}
+            ></BodyPost>
+          </div>
+        )
+      })
+    )
+  }
+}
+export default Post;
